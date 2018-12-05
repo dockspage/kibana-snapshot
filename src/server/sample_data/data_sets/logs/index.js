6fd082b58feef50cf7382e14d87ccf236cf6b29e
Anton <anton@adc.sh>
2018-12-05 17:39:52 +0300

    add logs

---
+++ src/server/sample_data/data_sets/logs/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.logsSpecProvider = logsSpecProvider;
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _saved_objects = require('./saved_objects');
+
+var _field_mappings = require('./field_mappings');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function logsSpecProvider() {
+  return {
+    id: 'logs',
+    name: 'Sample web logs',
+    description: 'Sample data, visualizations, and dashboards for monitoring web logs.',
+    previewImagePath: '/plugins/kibana/home/sample_data_resources/logs/dashboard.png',
+    overviewDashboard: 'edf84fe0-e1a0-11e7-b6d5-4dc382ef7f5b',
+    defaultIndex: '90943e30-9a47-11e8-b64d-95841ca0b247',
+    savedObjects: _saved_objects.savedObjects,
+    dataIndices: [{
+      id: 'logs',
+      dataPath: _path2.default.join(__dirname, './logs.json.gz'),
+      fields: _field_mappings.fieldMappings,
+      timeFields: ['timestamp'],
+      currentTimeMarker: '2018-08-01T00:00:00',
+      preserveDayOfWeekTimeOfDay: true
+    }]
+  };
+} /*
+   * Licensed to Elasticsearch B.V. under one or more contributor
+   * license agreements. See the NOTICE file distributed with
+   * this work for additional information regarding copyright
+   * ownership. Elasticsearch B.V. licenses this file to you under
+   * the Apache License, Version 2.0 (the "License"); you may
+   * not use this file except in compliance with the License.
+   * You may obtain a copy of the License at
+   *
+   *    http://www.apache.org/licenses/LICENSE-2.0
+   *
+   * Unless required by applicable law or agreed to in writing,
+   * software distributed under the License is distributed on an
+   * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+   * KIND, either express or implied.  See the License for the
+   * specific language governing permissions and limitations
+   * under the License.
+   */
