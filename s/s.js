d3170d77302cda8b860b1e65fc9b34a171b5a0cf
Anton <anton@adc.sh>
2018-12-05 14:54:16 +0300

    add missing dependencies

--- s/s.js
+++ s/s.js
@@ -1,6 +1,9 @@
 import spawn, { fork } from 'spawncommand'
 import cleanStack from '@artdeco/clean-stack'
 import { askSingle, confirm } from 'reloquent'
+import { dependencies, devDependencies } from './t.json'
+
+const DD = { ...dependencies, ...devDependencies }
 
 async function run() {
   const f = fork('src/cli', [], {
@@ -10,13 +13,16 @@ async function run() {
   const [,r] = /Cannot find module '(.+?)'/.exec(res.stderr) || []
   if (r) {
     console.log('MISSING %s', r)
-    const a = await confirm(`Install ${r}?`)
+    const version = DD[r]
+    if (!version) throw new Error('Unknown version')
+    const exact = `${r}@${version}`
+    const a = await confirm(`Install ${exact}?`)
     if (!a) return
-    console.log(' yarn add -E %s', r)
-    const p = spawn('yarn', ['add', '-E', r])
+    console.log(' yarn add -E %s', exact)
+    const p = spawn('yarn', ['add', '-E', exact])
     const rres = await p.promise
     if (rres.code != 0) throw new Error(rres.stderr)
-    await confirm(`${r} installed. Continue?`)
+    // await confirm(`${r} installed. Continue?`)
     await run()
   }
 }
