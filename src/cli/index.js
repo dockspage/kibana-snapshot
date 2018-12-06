4040f78e0d5624c0591eddfebb6c84f27b8ab479
Anton <anton@adc.sh>
2018-12-06 14:59:50 +0300

    pass prod

--- src/cli/index.js
+++ src/cli/index.js
@@ -21,7 +21,7 @@ const proxyServer = require('../../lib/proxy-server')
 const PROD = process.env.NODE_ENV == 'production'
 
 ;(async () => {
-  const url = await proxyServer()
+  const url = await proxyServer(PROD)
   console.log(
     'Proxy started on %s%s', url,
     PROD ? ' in production mode' : ''
